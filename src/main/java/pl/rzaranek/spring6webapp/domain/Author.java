0de292034ff415a1df08bae5118f83a724972fbe
Robert Zaranek <robert.zaranek@bilansjr.pl>
2023-12-03 17:31:56 +0100

    relationships Book and Author

--- src/main/java/pl/rzaranek/spring6webapp/domain/Author.java
+++ src/main/java/pl/rzaranek/spring6webapp/domain/Author.java
@@ -1,20 +1,31 @@
 package pl.rzaranek.spring6webapp.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.util.Set;
 
 /**
  * Created by robertZ on 2023-12-02.
  */
 @Entity
 public class Author {
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String firstName;
     private String lastName;
+
+    @ManyToMany(mappedBy = "authors")
+    private Set<Book> books;
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }
 
     public Long getId() {
         return id;
--- src/main/java/pl/rzaranek/spring6webapp/domain/Book.java
+++ src/main/java/pl/rzaranek/spring6webapp/domain/Book.java
@@ -1,20 +1,33 @@
 package pl.rzaranek.spring6webapp.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.util.Set;
 
 /**
  * Created by robertZ on 2023-12-02.
  */
 @Entity
 public class Book {
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String title;
     private String isbn;
+
+    @ManyToMany
+    @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
+            inverseJoinColumns = @JoinColumn(name = "author_id"))
+    private Set<Author> authors;
+
+    public Set<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Set<Author> authors) {
+        this.authors = authors;
+    }
 
     public Long getId() {
         return id;
